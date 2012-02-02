1f1529870b770696a917da116225da1b01f0a486
ccacioppo@gmail.com <ccacioppo@gmail.com>
2012-02-02 17:52:23 -0500

    first commit

---
+++ crunchbase-appsscript.js
@@ -0,0 +1,72 @@
+function cbsearch(company, field) {
+  var url = "http://api.crunchbase.com/v/1/company/" + String.toLowerCase(company) + ".js";
+  
+  try{
+    var response = UrlFetchApp.fetch(url);
+  
+    var companyInfo = Utilities.jsonParse(response.getContentText());
+
+    if (isGarbage(companyInfo) || isGarbage(companyInfo[field])) {
+      return "";
+    }
+  
+    return companyInfo[field];
+  }catch(error){
+    //catch 404 errors when company is not found in Crunchbase
+  }
+}
+
+function cbcity(company){
+  var url = "http://api.crunchbase.com/v/1/company/" + String.toLowerCase(company) + ".js";
+  
+  try{
+    var response = UrlFetchApp.fetch(url);
+
+    var companyInfo = Utilities.jsonParse(response.getContentText());
+
+    if (isGarbage(companyInfo) || isGarbage(companyInfo["offices"]) || isGarbage(companyInfo["offices"][0]) || isGarbage(companyInfo["offices"][0]["city"])){
+      return "";
+    } else {  
+      return companyInfo["offices"][0]["city"];  
+    }
+  }catch(error){
+    //catch 404 errors when company is not found in Crunchbase
+  }
+}
+
+function isGarbage(output){
+  if(output  == null || typeof output == null){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function monthword(month){
+  switch (month){
+    case 1:
+      return "January";
+    case 2: 
+      return "February";
+    case 3:
+      return "March";
+    case 4: 
+      return "April";
+    case 5:
+      return "May";
+    case 6: 
+      return "June";
+    case 7:
+      return "July";
+    case 8: 
+      return "August";
+    case 9:
+      return "September";
+    case 10: 
+      return "October";
+    case 11:
+      return "November";
+    case 12: 
+      return "December";
+  }
+}
